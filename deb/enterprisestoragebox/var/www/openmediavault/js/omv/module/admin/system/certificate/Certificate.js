cb370cee6ec790e55f432f1fb7d3c9ffe10fac0c
artio0610 <artio0610@gmail.com>
2015-11-04 10:15:21 +0000

    initial commit

---
+++ deb/enterprisestoragebox/var/www/openmediavault/js/omv/module/admin/system/certificate/Certificate.js
@@ -0,0 +1,30 @@
+/**
+ * This file is part of EnterpriSestorageBox.
+ *
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author   Artur Osinski-Stachowicz <artio0610@gmail.com>
+ * @copyright Copyright (c) 2009-2015 Artur Osinski-Stachowicz
+ *
+ * EnterpriSestorageBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * EnterpriSestorageBox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EnterpriSestorageBox. If not, see <http://www.gnu.org/licenses/>.
+ */
+// require("js/esb/WorkspaceManager.js")
+
+ESB.WorkspaceManager.registerNode({
+	id: "certificate",
+	path: "/system",
+	text: _("Certificates"),
+	icon16: "images/certificate.png",
+	iconSvg: "images/certificate.svg",
+	position: 60
+});
